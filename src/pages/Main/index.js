47fb30105f0c5b7dd4b5b4631f464cf74f4770b1
juuliana <jumaju10@hotmail.com>
2021-04-11 01:01:18 -0300

    input, errors, repositories --

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,37 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '~/services/api';
+import getRealm from '~/services/realm';
 
 import Repository from '~/components/Repository';
 
 import { Container, Title, Form, Input, Submit, List } from './styles';
 
 function Main() {
+  const [input, setInput] = useState('');
+  const [error, setError] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function loadRepositories(){
+      const realm = await getRealm();
+
+      const data = realm.objects('Repository').sorted('stars', true);
+      
+      setRepositories(data);
+    }
+
+    loadRepositories();
+  }, []);
+
+  async function saveRepository(repository){
+    const data = {
+      id: repository.id,
+      name: repository.name,
+      fullName: repository.full_name,
+      description: repository.description,
+      stars: repository.stargazers_count,
+      forks: repository.forks_count
+    };
+
+    const realm = await getRealm();
+
+    realm.write(() => {
+      realm.create('Repository', data);
+    });
+  }
+
+  async function handleAddRepository(){
+    try{
+      const response = await api.get(`/repos/${input}`);
+
+      await saveRepository(response.data);
+
+      setInput('');
+      setError(false);
+      Keyboard.dismiss();
+    }catch(err){
+      setError(true);
+    }
+
+  }
+
   return(
     <Container>
       <Title>Repositórios</Title>
 
       <Form>
         <Input
+          value={input}
+          error={error}
+          onChangeText={setInput}
           autoCapitalize="none"
           autoCorrect={false}
           placeholder="Procurar repositório..."
         />
-        <Submit onPress={() => {}}>
+        <Submit onPress={handleAddRepository}>
           <Icon name="add" size={22} color="#FFF"/>
         </Submit>
       </Form>
 
       <List
         keyboardShuldPersistTaps="handled"
-        data={[
-          {
-            id: 1,
-            name: "unform",
-            description: "React Native",
-            stars: 1234,
-            forks: 122
-          }
-        ]}
+        data={repositories}
         keyExtractor={item => String(item.id)}
         renderItem={({ item }) => (
           <Repository data={ item }/>
